152d8ee11447a07ada8cd322d77ad4acd7170b94
Utkarsh Mishra <UtkarshM@emids.com>
2023-02-06 10:22:29 +0530

    drag and drop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
+const data = [
+  {
+    id: "item-1",
+    content: "Item-1",
+  },
+  {
+    id: "item-2",
+    content: "Item-2",
+  },
+  {
+    id: "item-3",
+    content: "Item-3",
+  },
+  {
+    id: "item-4",
+    content: "Item-4",
+  },
+  {
+    id: "item-5",
+    content: "Item-5",
+  },
+  {
+    id: "item-6",
+    content: "Item-6",
+  },
+  {
+    id: "item-7",
+    content: "Item-7",
+  },
+  {
+    id: "item-8",
+    content: "Item-8",
+  },
+  {
+    id: "item-9",
+    content: "Item-9",
+  },
+  {
+    id: "item-10",
+    content: "Item-10",
+  },
+];
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: 8,
+  width: 250,
+});
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  userSelect: "none",
+  padding: 16,
+  margin: "0 0 8px 0",
+  background: isDragging ? "lightGreen" : "grey",
+  ...draggableStyle,
+});
+const App = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    setItems(data);
+  }, []);
+
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return;
+    }
+    const reorderedItems = reorder(
+      items,
+      result.source.index,
+      result.destination.index
+    );
+
+    // axios.post().then(setItems(reorderedItems))  
+    console.log(reorderedItems);
+    setItems(reorderedItems);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)}
+            >
+              {items.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      className="card"
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                    >
+                      {item.content}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
-}
+};
 
 export default App;
